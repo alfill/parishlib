0a24ae5b93b94bb8df857986298c41d35f231c76
alfill <7522884+alfill@users.noreply.github.com>
2019-04-19 21:09:08 +0500

    hello word\!)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# parishlib
-Книжный каталог
+###Книжный каталог
+На основе [DataTables](https://datatables.net/) плагина к jQuery, создан Allan Jardine, SpryMedia Ltd, Шотландия        
+Локализация: [Alexander Pyatkin](https://github.com/DataTables/Plugins/blob/master/i18n/Russian.lang)
---
+++ interface.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+    var table = $('#example').DataTable({
+        //"data": testdata.jsondata,
+        "ajax": "http://127.0.0.1:8887/booklist-data.json",
+        //"ajax": "https://api.myjson.com/bins/tpzqc",   
+        //"ajax": "booklist-data.json",
+        responsive: false,
+        ordering: false,
+        lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "∞"]],
+        pageLength: 25,
+        fixedHeader: true,
+        info: true,
+        //select:"single",
+        columns: [{
+                "className": 'details-control',
+                "orderable": false,
+                "data":      null,
+                "defaultContent": '',
+                /*,
+                               "render": function () {
+                                   return '<i class="fa fa-plus-square" aria-hidden="true"></i>';
+                               },  */
+                width: "15px",
+                height: "15px"
+            },
+           // { "data": "name" },      // title
+           // { "data": "position" },     // author
+           // { "data": "office" },  // publisher
+           // { "data": "salary" }       //  year
+            { "data": "title" },      // title
+            { "data": "author" },     // author
+            { "data": "publisher" },  // publisher
+            { "data": "year" }       //  year
+            //{ "data": "part" }        //
+        ],
+        //"order": [[1, 'asc']],
+        rowGroup: { dataSrc: "part" },
+
+        language: {
+            "processing": "Подождите...",
+            "search": "Поиск:",
+            "lengthMenu": "Показано _MENU_ записей",
+            "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+            "infoEmpty": "Записи с 0 до 0 из 0 записей",
+            "infoFiltered": "(отфильтровано из _MAX_ записей)",
+            "infoPostFix": "",
+            "loadingRecords": "Загрузка записей...",
+            "zeroRecords": "Записи отсутствуют.",
+            "emptyTable": "В таблице отсутствуют данные",
+            "paginate": {
+                "first": "Первая",
+                "previous": "«--",
+                "next": "--»",
+                "last": "Последняя"
+            },
+            "aria": {
+                "sortAscending": ": активировать для сортировки столбца по возрастанию",
+                "sortDescending": ": активировать для сортировки столбца по убыванию"
+            }
+        }
+    });
+
+    // Add event listener for opening and closing details
+    $('#example tbody').on('click', 'td.details-control', function() {
+        var tr = $(this).closest('tr');
+        var row = table.row(tr);
+
+        if (row.child.isShown()) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        } else {
+            // Open this row
+            row.child(format(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
+
+    table.on("user-select", function(e, dt, type, cell, originalEvent) {
+        if ($(cell.node()).hasClass("details-control")) {
+            e.preventDefault();
+        }
+    });
+});
+
+function format(d) {
+    // `d` is the original data object for the row
+    var trans = '<tr>' + '<td>Издание/Перевод: </td>' + 
+                '<td>' + d.translate + '</td>' +
+                '</tr>';
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        
+        trans + 
+
+        '<tr>' +
+          '<td>Серия: </td>' + '<td>' + d.classification + '</td>' +
+        '</tr>' +
+        '<tr>' +
+          '<td>Аннотация: </td>' + '<td>' + d.annotation + '</td>' +
+        '</tr>' +
+        '</table>';
+    }
+
+
+    /*
+
+        '<tr>' +
+          '<td>Издание/Перевод: </td>' + '<td>' + d.translate + '</td>' +
+        '</tr>' +
+
+
+    */
